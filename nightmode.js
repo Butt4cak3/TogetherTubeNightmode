84294d334e2879b1e8c2718ced4b2ef3d4de106e
Butt4cak3 <butt4cak3@gmail.com>
2017-02-12 00:11:54 +0100

    Added big player toggle

--- nightmode.js
+++ nightmode.js
@@ -1,28 +1,39 @@
 (function () {
-	var menu = document.querySelector(".navbar-room-header .navbar-right"),
-		li = document.createElement("li"),
-		a = document.createElement("a"),
-		i = document.createElement("i");
+	var menu = document.querySelector(".navbar-room-header .navbar-right");
 
+	addToolbarItem("Big player", "arrows-alt", toggleBigPlayer);
+	addToolbarItem("Toggle Nightmode", "lightbulb-o", toggleNightMode);
 
-	menu.appendChild(li);
+	function addToolbarItem(label, icon, onClick) {
+		var li = document.createElement("li"),
+			a = document.createElement("a"),
+			i = document.createElement("i");
 
-	li.appendChild(a);
+		menu.appendChild(li);
 
-	a.href = "#";
-	a.addEventListener("click", function () {
-		toggleNightMode();
-	});
-	a.appendChild(i);
-	a.appendChild(document.createTextNode(" Toggle Nightmode"));
+		li.appendChild(a);
 
-	i.classList.add("fa", "fa-lightbulb-o");
+		a.href = "#";
+		a.addEventListener("click", onClick);
+		a.appendChild(i);
+		a.appendChild(document.createTextNode(" " + label));
 
+		i.classList.add("fa", "fa-" + icon);
+	}
+	
 	function toggleNightMode() {
 		if (document.body.classList.contains("nightmode")) {
 			document.body.classList.remove("nightmode");
 		} else {
 			document.body.classList.add("nightmode");
+		}
+	}
+
+	function toggleBigPlayer() {
+		if (document.body.classList.contains("bigplayer")) {
+			document.body.classList.remove("bigplayer");
+		} else {
+			document.body.classList.add("bigplayer");
 		}
 	}
 }());
